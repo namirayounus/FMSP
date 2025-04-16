c1b8c68be48902f4efbe37350a02f107cefeb5d4
Namira <namirayounus@gmail.com>
2025-04-16 08:55:09 +0530

    Fix NameError by importing django.forms in views.py

--- farm/views.py
+++ farm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django import forms
 from .models import Profile
 from .forms import ProfileForm, WorkerCreationForm
 
